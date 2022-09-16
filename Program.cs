f390a80e3a4fe8e1fa539fa5ccc5f45e116f2f75
@igor_mashkin <igor-190677@yandex.ru>
2022-09-16 21:42:15 +0300

    Add Program.cs in final homework

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿string[] FindItems(string[] arrayUser)
+{
+    string[] newArray = new string[0];
+    // int counter = 0;
+    for (int i = 0; i < arrayUser.Length; i++)
+    {
+        if (arrayUser[i].Length <= 3)
+        {
+            newArray = newArray.Concat(new string[] {arrayUser[i]}).ToArray();
+            // newArray[counter] = arrayUser[i];
+            // counter++;
+        }
+    }
+    return newArray;
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i == array.Length - 1)
+            Console.Write($"{array[i]}");
+        else
+            Console.Write($"{array[i]}, ");
+    }
+}
+
+string[] array = new string[5] { "Hello", "World", "I", "am", "Igor"};
+
+string[] newArrayUser = FindItems(array);
+Console.WriteLine("Исходный массив: ");
+PrintArray(array);
+Console.WriteLine();
+Console.WriteLine();
+Console.WriteLine("Новый массив: ");
+PrintArray(newArrayUser);
